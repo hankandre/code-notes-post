0fc5f3659a9349ea969df616deb1bd7bead57c87
Hank Andre <hank.andre@irishtitan.com>
2017-04-13 01:09:05 -0500

    completed refactoring

--- app.js
+++ app.js
@@ -4,34 +4,32 @@ const handler = require('./handler')
 const micro = require('micro')
 const microApi = require('micro-api')
 
-const app = micro(async (req, res) => {
-  microApi([
-    {
-      method: 'get',
-      path: '/',
-      handler: handler.all(req, res)
-    },
-    {
-      method: 'get',
-      path: '/:id',
-      handler: handler.find(req, res)
-    },
-    {
-      method: 'post',
-      path: '/',
-      handler: handler.save(req, res)
-    },
-    {
-      method: 'put',
-      path: '/',
-      handler: handler.update(req, res)
-    },
-    {
-      method: 'delete',
-      path: '/:id',
-      handler: handler.remove(req, res)
-    }
-  ])
-})
+const app = microApi([
+  {
+    method: 'get',
+    path: '/:id',
+    handler: handler.find
+  },
+  {
+    method: 'get',
+    path: '/',
+    handler: handler.all
+  },
+  {
+    method: 'post',
+    path: '/',
+    handler: handler.save
+  },
+  {
+    method: 'put',
+    path: '/',
+    handler: handler.update
+  },
+  {
+    method: 'delete',
+    path: '/:id',
+    handler: handler.remove
+  }
+])
 
-app.listen(3000)
+module.exports = app
--- handler.js
+++ handler.js
@@ -1,55 +1,44 @@
 'use strict'
 const Post = require('./PostSchema')
-const { send, json} = require('micro')
-const querystring = require('querystring')
+const { send } = require('micro')
 
 module.exports = {
-  find: find,
-  all: all,
-  save: save,
-  update: update,
-  remove: remove
+  find,
+  all,
+  remove,
+  update,
+  save
 }
 
-async function find ({body}) {
-  const id = ctx.params.id
+async function find ({params: { id }}) {
+  console.log(id)
   const doc = await Post.findById(id)
-  ctx.body = doc
+  return doc
 }
 
-async function all (req, res) {
-  req.query = querystring.parse(req.url.substring(2))
-  console.log(req.query)
-  const take = parseInt(req.query.take)
-  const skip = parseInt(req.query.skip)
-  console.log(take, skip)
-
+async function all ({params: {skip, take}}) {
   return {
-    posts: await Post.find({isDeleted: false}).skip(skip).limit(take),
+    posts: await Post.find({isDeleted: false}).skip(parseInt(skip)).limit(parseInt(take)),
     total: await Post.find().count()
   }
 }
 
-async function save (ctx) {
-  const body = ctx.request.body
+async function save ({body}) {
   const post = new Post(body)
   const doc = await post.save()
-  console.log(doc)
-  ctx.body = doc
+  return doc
 }
 
-async function update (ctx) {
-  const body = ctx.request.body
+async function update ({body}) {
   const doc = await Post.findByIdAndUpdate(body._id, body, {new: true})
-  ctx.body = doc
+  return doc
 }
 
-async function remove (ctx) {
+async function remove ({res, params: {id}}) {
   try {
-    const id = ctx.params.id
     const doc = await Post.findByIdAndUpdate(id, {isDeleted: true}, {new: true}).exec()
-    ctx.body = doc
+    return doc
   } catch (err) {
-    ctx.throw(404, err)
+    send(res, 404, {error: 'document not found'})
   }
 }
