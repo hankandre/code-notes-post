4618679d2e58df8ad3555c53f56260eb83ef8c42
Hank Andre <hank.andre@irishtitan.com>
2017-05-08 08:19:46 -0500

    adding default image to post

--- PostSchema.js
+++ PostSchema.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const PostSchema = new Schema({
   title: { type: String, required: true },
+  image: { type: String, default: 'https://static.pexels.com/photos/389818/pexels-photo-389818.jpeg' },
   content: { type: String, required: true },
   type: {type: String, required: true},
   tags: [{ type: String }],
